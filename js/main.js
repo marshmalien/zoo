0cec283d1e3de74a3b5fa82cb6b55ca254b6b1dd
Marliana Lara <marliana.lara@gmail.com>
2016-10-02 11:56:58 -0400

    add reproduce function

--- js/main.js
+++ js/main.js
@@ -1,67 +1,56 @@
-$(document).ready(function() {
-  console.log("WE IN");
-
-  $('.bat').click(function() {
-    console.log('You clicked on the bat');
-
-  });
-
-});
-
 // Animal constructor
 function Animal(name, dateOfBirth) {
   this.name = name;
   this.dateOfBirth = dateOfBirth;
   this.species = "unknown";
-
+  this.type = "unknown";
   this.regions = {
     container: null,
     infoContainer: null,
     theAnimal: null
   };
-
   this.updateInfo = function() {
     this.regions.infoContainer.innerHTML = '';
-    this.addInfo(this.toString());
+    this.addInfo(this.displayIntro());
   };
 }
 
 // Animal prototype... ADD METHODS TO PROTOTYPE
-// Animal.prototype.myName = function() {
-//     console.log("My name is " + this.name + ".");
-// };
 Animal.prototype.ageInYears = function(dateOfBirth) {
   var currentYear = new Date().getFullYear();
   var birthYear = this.dateOfBirth.split("/")[2];
   return currentYear - birthYear;
 };
-Animal.prototype.saySpecies = function() {
-  var speak = "I'm a " + this.species;
-  return speak;
+Animal.prototype.reproduce = function() {
+  if (this.type === "mammal") {
+    return "can give birth";
+  } else {
+    return "can lay eggs"
+  }
 };
-
-Animal.prototype.age = function() {
-  return " I am " + this.ageInYears() + " years old.";
+Animal.prototype.displayIntro = function() {
+  return "My name is " + this.name + ", and I'm a " + this.ageInYears() + " year old " + this.species + ". I'm also a " + this.type + " and " + this.reproduce() + ".";
 };
-
+Animal.prototype.toString = function() {
+  return "Animal: " + "name=" + this.name + ", dateOfBirth=" + this.dateOfBirth + ", species=" + this.species + ", type=" + this.type;
+};
 Animal.prototype.buildMe = function() {
   var bodyContainer = document.getElementById('container'),
-      container = document.createElement('div'),
-      infoContainer = document.createElement('div'),
-      theAnimal = document.createElement('div');
+    container = document.createElement('div'),
+    infoContainer = document.createElement('div'),
+    theAnimal = document.createElement('div');
 
-      container.className = "animalContainer";
-      infoContainer.className = "infoContainer";
+  container.className = "animalContainer";
+  infoContainer.className = "infoContainer";
+  theAnimal.className = this.species;
 
-      theAnimal.className = this.species;
+  container.appendChild(theAnimal);
+  container.appendChild(infoContainer);
+  bodyContainer.appendChild(container);
 
-      container.appendChild(theAnimal);
-      container.appendChild(infoContainer);
-      bodyContainer.appendChild(container);
-
-      this.regions.container = container;
-      this.regions.infoContainer = infoContainer;
-      this.regions.theAnimal = this.species;
+  this.regions.container = container;
+  this.regions.infoContainer = infoContainer;
+  this.regions.theAnimal = this.species;
 };
 Animal.prototype.addInfo = function(content) {
   var info = document.createElement('p');
@@ -70,53 +59,36 @@ Animal.prototype.addInfo = function(content) {
 };
 
 // Elephant constructor
-function Elephant(name, dateOfBirth, isMammal) {
+function Elephant(name, dateOfBirth) {
   Animal.call(this, name, dateOfBirth);
-  this.isMammal = isMammal;
   this.species = "elephant";
+  this.type = "mammal";
 }
 // Elephant prototype
 Elephant.prototype = Object.create(Animal.prototype);
 Elephant.prototype.hobbies = function(hobby) {
   return " I like to " + hobby + " in my free time.";
 };
-Elephant.prototype.mammal = function() {
-  return (this.isMammal ? " I'm also a mammal." : "I'm not a mammal.");
-};
 
 var elephant = new Elephant("Hugo", "08/20/2014", "true");
-Elephant.prototype.toString = function() {
-  var intro = "My name is " + this.name + ". " + this.saySpecies();
-  var mammal = " and " + this.mammal();
-  var age = this.age();
-  var hobby = this.hobbies("paint");
-  return intro + mammal + age + hobby;
-};
+
 elephant.buildMe();
 elephant.updateInfo();
 
 // Snake constructor
-function Snake(name, dateOfBirth, isReptile, isVenomous) {
+function Snake(name, dateOfBirth, isVenomous) {
   Animal.call(this, name, dateOfBirth);
-  this.isReptile = isReptile;
   this.isVenomous = isVenomous;
   this.species = "snake";
+  this.type = "reptile";
 }
 // Snake prototype.
 Snake.prototype = Object.create(Animal.prototype);
-Snake.prototype.layEggs = function(numberOfEggs) {
-  return " I laid " + numberOfEggs +" eggs yesterday.";
-};
 Snake.prototype.prey = function(food) {
-    return " My favorite food " + food;
+  return " My favorite food " + food;
 };
 Snake.prototype.toString = function() {
-   var intro = " My name is " + this.name + ". " + this.saySpecies();
-   var venom = " and I'm " + (this.isVenomous ? "" : "not") + " venomous! ";
-   var age = this.age();
-   var eggs = (this.isReptile ? " I'm a reptile and " +  this.layEggs(5) : "I'm not a reptile.");
-   var food = this.prey(" is caught locally, mice!");
-   return intro + venom + age + eggs + food;
+  return Animal.prototype.toString.call(this) + ", isVenomous=" + this.isVenomous;
 };
 
 var snake = new Snake('Slinky', "09/07/1989", 'true');
@@ -124,25 +96,17 @@ snake.buildMe();
 snake.updateInfo();
 
 // Bat constructor
-function Bat(name, dateOfBirth, isMammal) {
+function Bat(name, dateOfBirth) {
   Animal.call(this, name, dateOfBirth);
-  this.isMammal = isMammal;
   this.species = "bat";
+  this.type = "mammal";
 }
 Bat.prototype = Object.create(Animal.prototype);
-Bat.prototype.mammal = function() {
-  return (this.isMammal ? " I'm a mammal" : "I'm not a mammal.");
-};
+
 Bat.prototype.fly = function() {
   return " Flying is my specialty!";
 };
-Bat.prototype.toString = function() {
-  var intro = "My name is " + this.name + " and " + this.saySpecies() + ".";
-  var age = this.age();
-  var mammal = this.mammal() + " and my significant other gave birth to babies last year.";
-  var fly = this.fly();
-  return intro + age + mammal + fly;
-};
+
 var bat = new Bat('B-Man', '03/04/1947', 'true');
 bat.buildMe();
 bat.updateInfo();
